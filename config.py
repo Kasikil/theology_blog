396589939f02da726456dbefc9106bcedc0b25c9
Clayt <clayton.wwjd@gmail.com>
2020-03-28 23:58:21 -0500

    Tutorial Part 4: Database - Users, Post, Flask Shell, Migrations

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess-Psweet'
+    # <DB TYPE>+<DB interface?>://<username>:<password>@<location>/<database>
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/theology_blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
